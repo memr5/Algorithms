074a6013fa90492a1041759566e6672a0270fd29
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2019-10-08 11:17:19 +0530

    Added insertion sort algorithm in c

---
+++ Sorting Algorithms/C-CPP/insertion_sort.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int data[100],n,temp,i,j;
+printf("Enter number of terms(should be less than 100): ");
+scanf("%d",&n);
+printf("Enter elements: ");
+for(i=0;i<n;i++)
+{
+scanf("%d",&data[i]);
+}
+for(i=1;i<n;i++)
+{
+temp = data[i];
+j=i-1;
+while(temp<data[j] && j>=0)
+/*To sort elements in descending order, change temp<data[j] to temp>data[j] in above line.*/
+{
+data[j+1] = data[j];
+--j;
+}
+data[j+1]=temp;
+}
+printf("In ascending order: ");
+for(i=0; i<n; i++)
+printf("%d\t",data[i]);
+return 0;
+}
