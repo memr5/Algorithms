7e53540f944bbf74d1d179ed14c1f537d07699eb
Roberto Torella <robertot@eu.square-enix.com>
2019-10-09 12:49:43 +0100

    Added tests for HammingDistance

--- Hamming/hamming_test.go
+++ Hamming/hamming_test.go
@@ -11,3 +11,9 @@ func TestHammingWeight(t *testing.T) {
 	assert.Equal(t, HammingWeight(0xabcdef), 17)
 	assert.Equal(t, HammingWeight(0xabcdef1234567890), 32)
 }
+
+func TestHammingDistance(t *testing.T) {
+	assert.Equal(t, HammingDistance(0x1234567890, 0xabcdef), 24)
+	assert.Equal(t, HammingDistance(0xabcdef, 0xabcdef1234567890), 41)
+	assert.Equal(t, HammingDistance(0xabcdef1234567890, 0x1234567890), 17)
+}
