9ec2e25c40ef35cfe6c8d6a9c359893757943f59
rishika1802 <rishikaroy1802@gmail.com>
2019-10-11 15:47:53 +0530

    Add Mirror Tree Code

---
+++ Binary Tree/MirrorTree.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+void mirror(struct Node* node);
+
+void inOrder(struct Node* node)
+{
+  if (node == NULL)
+    return;
+  inOrder(node->left);
+  printf("%d ", node->data);
+  inOrder(node->right);
+}
+
+int main()
+{
+  int t;
+  struct Node *child;
+  scanf("%d", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = new Node(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     mirror(root);
+     inOrder(root);
+     cout << endl;
+  }
+  return 0;
+}
+
+
+
+void mirror(Node* root) 
+{
+    if(root==NULL)
+    return;
+    else{
+    Node *temp;
+    mirror(root->left);
+    mirror(root->right);
+    temp=root->left;
+    root->left=root->right;
+    root->right=temp;
+    }
+
+}
