8ab92f33831c18442813d3f3bde541a691dfd7fd
Leonardo Su <leonardo.su@ccc.ufcg.edu.br>
2019-10-10 22:40:04 -0300

    Update BIT.cpp

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h> //Fenwick (Binary Indexed) Trees 
 using namespace std;    //Leonardo Su
-					   // Github : @Leonardosu
+		       // Github : @Leonardosu
                       // Hacktoberfest
 
 typedef long long int ll;
