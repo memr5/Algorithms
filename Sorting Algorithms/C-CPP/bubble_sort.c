abffa7443224b3ebe8fabe38e2866e6a7ebef9df
Maria Luísa Mendes <72216924+lumendesp@users.noreply.github.com>
2020-10-13 23:19:17 -0300

    Update bubble_sort.c
    Added the option to choose the size of the array, choose the elements and the ordering.

--- Sorting Algorithms/C-CPP/bubble_sort.c
+++ Sorting Algorithms/C-CPP/bubble_sort.c
@@ -1,35 +1,67 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void bbSort(int arr[], int N)       //Function for Bubble Sorting
- {
-    int i, j, aux;
-    for (i = 1; i < N; i++)
-    for (j = 0; j < N - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-            aux = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = aux;
-        }
-    }
+void swap (int array[], int j){
+  int aux = 0;
+  aux = array[j]; 
+  array[j] = array[j+1]; 
+  array[j+1] = aux;
 }
 
-int main()
-{
-    int N, aux;
-    printf("\nEnter the number of elements= ");
-    scanf(" %d", &N);
-    int arr[N];
-    printf("\nEnter the elements= ");
-    for(int i = 0; i < N; i++)
-	{
-        scanf(" %d", &aux);
-        arr[i] = aux;
+void bubbleSort(int array[], int size, int order){
+  if(order == 1){
+    for(int i=0; i < size-1; i++){
+      int flag = 0;
+
+      for(int j=0; j<size-1-i; j++){
+        if(array[j] > array[j+1]){
+          swap(array, j);
+          flag = 1;
+        }
+      }
+      if(flag == 0){
+        break;
+      }
     }
-    bbSort(arr, N);
-    printf("\nThe sorted elements= \n");
-    for(int i = 0; i < N; i++)
-    {
-        printf("\n%d", arr[i]);
+  }
+
+  else if(order == 2){
+    for(int i=0; i < size-1; i++){
+      int flag = 0;
+
+      for(int j=0; j<size-1-i; j++){
+        if(array[j] < array[j+1]){
+          swap(array, j);
+          flag = 1;
+        }
+      }
+      if(flag == 0){
+        break;
+      }
     }
-    return 0;
+  }
+}
+int main() {
+  int size;
+  int order;
+
+  printf("Enter the size of the array:\n");
+	scanf("%i", &size);
+
+	int array[size];
+
+	printf("Enter the elements of the array:\n");
+	for(int i = 0; i < size; i++){
+	  scanf("%i", &array[i]);
+  }
+
+  printf("What type of ordering do you want: \n 1 - Ascending \n 2 - Descending\n");
+  scanf("%i", &order);
+
+	bubbleSort(array, size, order);
+
+	printf("The sorted array is:\n");
+	for (int i = 0; i < size; i++){
+    printf("%i ", array[i]);
+  }
+  return 0;
 }
